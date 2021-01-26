9df0ef72b39e11cba13e85af932e10ea9a80a63d
NeOMakinG <valentin.szczupak@prestashop.com>
2021-01-26 13:26:04 +0100

    Adjust for the UIKit, remove unused files

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -4,6 +4,7 @@ import * as React from 'react';
 import { ColorSchemeName } from 'react-native';
 
 import NotFoundScreen from '../screens/NotFoundScreen';
+import UIKitScreen from '../screens/UIKit';
 import { RootStackParamList } from '../types';
 import BottomTabNavigator from './BottomTabNavigator';
 import LinkingConfiguration from './LinkingConfiguration';
@@ -28,6 +29,7 @@ function RootNavigator() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Root" component={BottomTabNavigator} />
+      <Stack.Screen name="UIKit" component={UIKitScreen} options={{ title: 'UIKit examples' }} />
       <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
     </Stack.Navigator>
   );
--- screens/UIKit.tsx
+++ screens/UIKit.tsx
@@ -5,7 +5,7 @@ import { Text, View } from '../components/UIKit/Themed';
 export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
+      <Text style={styles.title}>UIKit</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
     </View>
   );
